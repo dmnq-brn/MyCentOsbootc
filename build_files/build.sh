9d0ea9be0964c2f86dcf4966ac8c83162e37a323
dmnq-brn <dominique.baron@cs-soprasteria.com>
2026-07-26 22:11:48 +0200

    Update package list according errors during build

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,13 +15,17 @@ cp -avf "/ctx/system_files"/. /
 # this installs a package from fedora repos
 # dnf5 install -y tmux
 dnf -y --setopt=install_weak_deps=False group install GNOME
-dnf -y install NetworkManager-openconnect.x86_64 NetworkManager-openconnect-gnome.x86_64 NetworkManager-openvpn.x86_64 NetworkManager-openvpn-gnome.x86_64 \
-NetworkManager-wifi.x86_64 audit.x86_64 bpftool.x86_64 dnsmasq.x86_64 firewalld.noarch git-core.x86_64 git-core-doc.noarch \
+dnf -y install audit.x86_64 bpftool.x86_64 dnsmasq.x86_64 firewalld.noarch git-core.x86_64 git-core-doc.noarch \
 hunspell.x86_64 hunspell-fr.noarch hyperv-daemons.x86_64 ibus-typing-booster.noarch iw iwlwifi-dvm-firmware.noarch iw.x86_64 iwlwifi-mvm-firmware.noarch \
 javascriptcoregtk4.1.x86_64 kernel-modules-extra.x86_64 mcelog.x86_64 nm-connection-editor.x86_64 open-vm-tools.x86_64 open-vm-tools-desktop.x86_64 \
 openconnect.x86_64 openvpn.x86_64 pcsc-lite.x86_64 pinentry.x86_64 qemu-guest-agent.x86_64 rclone.x86_64 realmd.x86_64 rsync.x86_64 smartmontools.x86_64 \
 vim-enhanced.x86_64 xdg-terminal-exec.noarch
-# dnf -y install restic.x86_64
+# Should exist somwhere to be added later
+# dnf -y install NetworkManager-openconnect.x86_64 NetworkManager-openconnect-gnome.x86_64 NetworkManager-openvpn.x86_64 NetworkManager-openvpn-gnome.x86_64 \
+# NetworkManager-wifi.x86_64
+
+# useful for secure archive.
+# dnf -y install restic.x86_64 
 dnf -y remove baobab.x86_64 gnome-calculator.x86_64 gnome-characters.x86_64 gnome-clocks.x86_64 gnome-color-manager.x86_64 papers.x86_64 \
 gnome-font-viewer.x86_64 gnome-remote-desktop.x86_64 gnome-system-monitor.x86_64 \
 gnome-text-editor.x86_64 gnome-tour.x86_64 gnome-user-docs.noarch sane-backends.x86_64 loupe.x86_64 subscription-manager.x86_64
