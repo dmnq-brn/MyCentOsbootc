1544de664fa31c4759c601a280e794757862a2e4
dmnq-brn <dominique.baron@cs-soprasteria.com>
2026-07-28 09:03:31 +0200

    add correct dnf comand

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,23 +16,22 @@ cp -avf "/ctx/system_files"/. /
 # dnf5 install -y tmux
 
 # Gnome minimal desktop
-dnf -y --setopt=install_weak_deps=False NetworkManager-wifi PackageKit-command-not-found PackageKit-gtk3-module audit at-spi2-atk at-spi2-core avahi avahi-tools bpftool  dconf \
-    centos-backgrounds dnsmasq firewalld.noarch fprintd-pam git-core git-core-doc gdm glib-networking  glibc-all-langpacks gnome-bluetooth gnome-browser-connector  \
+dnf -y --setopt=install_weak_deps=False install NetworkManager-wifi PackageKit-command-not-found PackageKit-gtk3-module audit at-spi2-atk at-spi2-core avahi avahi-tools bpftool  \
+    centos-backgrounds dconf dnsmasq firewalld.noarch fprintd-pam git-core git-core-doc gdm glib-networking  glibc-all-langpacks gnome-bluetooth gnome-browser-connector  \
     gnome-color-manager gnome-control-center gnome-disk-utility gnome-font-viewer gnome-initial-setup gnome-session-wayland-session gnome-settings-daemon \
     gnome-shell gnome-shell-extension-background-logo gnome-software gvfs-fuse gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-smb iw iwlwifi-dvm-firmware.noarch \
-    iw.x86_64 iwlwifi-mvm-firmware.noarchlibcanberra-gtk3 librsvg2 mesa-dri-drivers mesa-vulkan-drivers nautilus nfs-utils orca plymouth-system-theme polkit ptyxis  \
-    snapshot tracker tracker-miners xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-user-dirs-gtk yelp-tools vim-enhanced.x86_64
+    iw.x86_64 iwlwifi-mvm-firmware.noarchlibcanberra-gtk3 librsvg2 mesa-dri-drivers mesa-vulkan-drivers nautilus nfs-utils orca plymouth-system-theme polkit ptyxis rsync \
+    smartmontools.x86_64 snapshot tracker tracker-miners xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-user-dirs-gtk yelp-tools vim-enhanced.x86_64
 
 # Fonts
-dnf -y --setopt=install_weak_deps=False default-fonts-cjk-mono default-fonts-cjk-sans default-fonts-cjk-serif default-fonts-core-emoji default-fonts-core-math \
+dnf -y --setopt=install_weak_deps=False install default-fonts-cjk-mono default-fonts-cjk-sans default-fonts-cjk-serif default-fonts-core-emoji default-fonts-core-math \
    default-fonts-core-mono default-fonts-core-sans default-fonts-core-serif default-fonts-other-mono default-fonts-other-sans default-fonts-other-serif dejavu-sans-fonts \
    dejavu-sans-mono-fonts dejavu-serif-fonts google-carlito-fonts google-crosextra-caladea-fonts google-droid-sans-fonts google-droid-sans-mono-fonts \
    google-droid-serif-fonts google-noto-emoji-fonts google-noto-fonts-all google-noto-sans-cjk-fonts google-roboto-slab-fonts pt-sans-fonts \
    redhat-display-vf-fonts redhat-mono-vf-fonts redhat-text-vf-fonts
 
 # Guest Desktop Agents
-dnf -y hyperv-daemons.x86_64 open-vm-tools.x86_64 open-vm-tools-desktop.x86_64 \
-    qemu-guest-agent.x86_64 realmd.x86_64 rsync.x86_64 smartmontools.x86_64
+dnf -y --setopt=install_weak_deps=False install hyperv-daemons.x86_64 open-vm-tools.x86_64 open-vm-tools-desktop.x86_64 qemu-guest-agent.x86_64 realmd.x86_64
 
 # shall be installed from EPEL repository to be added later
 # dnf -y install NetworkManager-openconnect.x86_64 NetworkManager-openconnect-gnome.x86_64 NetworkManager-openvpn.x86_64 NetworkManager-openvpn-gnome.x86_64
