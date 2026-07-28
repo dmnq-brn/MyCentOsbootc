d32fdc3a01773a6c861ae30e0745a95e212e7c30
dmnq-brn <dominique.baron@cs-soprasteria.com>
2026-07-28 10:02:15 +0200

    remove gnome-font-viewer and snapshot packages from installation list remove following installed pakages isubscription-manager and subscription-manager-rhsm-certificates

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,10 +18,10 @@ cp -avf "/ctx/system_files"/. /
 # Gnome minimal desktop
 dnf -y --setopt=install_weak_deps=False install NetworkManager-wifi PackageKit-command-not-found PackageKit-gtk3-module audit at-spi2-atk at-spi2-core avahi avahi-tools bpftool  \
     centos-backgrounds dconf dnsmasq firewalld.noarch fprintd-pam git-core git-core-doc gdm glib-networking  glibc-all-langpacks gnome-bluetooth gnome-browser-connector  \
-    gnome-color-manager gnome-control-center gnome-disk-utility gnome-font-viewer gnome-initial-setup gnome-session-wayland-session gnome-settings-daemon \
+    gnome-color-manager gnome-control-center gnome-disk-utility gnome-initial-setup gnome-session-wayland-session gnome-settings-daemon \
     gnome-shell gnome-shell-extension-background-logo gnome-software gvfs-fuse gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-smb iw iwlwifi-dvm-firmware.noarch \
     iw.x86_64 iwlwifi-mvm-firmware libcanberra-gtk3 librsvg2 mesa-dri-drivers mesa-vulkan-drivers nautilus orca plymouth-system-theme polkit ptyxis rsync realmd \
-    smartmontools.x86_64 snapshot tracker tracker-miners xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-user-dirs-gtk yelp-tools vim-enhanced.x86_64
+    smartmontools.x86_64 tracker tracker-miners xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-user-dirs-gtk yelp-tools vim-enhanced.x86_64
 
 # Fonts
 dnf -y --setopt=install_weak_deps=False install default-fonts-cjk-mono default-fonts-cjk-sans default-fonts-cjk-serif default-fonts-core-emoji default-fonts-core-math \
@@ -32,6 +32,9 @@ dnf -y --setopt=install_weak_deps=False install default-fonts-cjk-mono default-f
 
 # Guest Desktop Agents
 dnf -y --setopt=install_weak_deps=False install hyperv-daemons open-vm-tools-desktop qemu-guest-agent spice-vdagent
+
+#  Pacakges to remove
+dnf -y remove subscription-manager subscription-manager-rhsm-certificates
 
 # shall be installed from EPEL repository to be added later
 # dnf -y install NetworkManager-openconnect.x86_64 NetworkManager-openconnect-gnome.x86_64 NetworkManager-openvpn.x86_64 NetworkManager-openvpn-gnome.x86_64
